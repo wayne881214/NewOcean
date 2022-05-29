70425c6e80725c3dff186ef1116226abb3563a7b
bobo <pauleric789456@gmail.com>
2022-05-29 19:41:29 +0800

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:newocean/storage_service.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -8,13 +9,11 @@ void main() async{
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: HomeScreen(),
@@ -25,15 +24,6 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -41,21 +31,21 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final Storage storage =Storage();
     return Scaffold(
       appBar: AppBar(
         title: Text('cloud storage'),
       ),
       body: Column(
-    children: [
-      Center(
-    child: ElevatedButton(
-    onPressed: ()async{
-      final results = await FilePicker.platform.pickFiles(
-        allowMultiple: false,
-        type:FileType.custom,
-        allowedExtensions: ['png','jpg'],
-      );
-
+        children: [
+         Center(
+           child: ElevatedButton(
+            onPressed: () async {
+              final results = await FilePicker.platform.pickFiles(
+              allowMultiple: false,
+              type:FileType.custom,
+               allowedExtensions: ['png','jpg'],
+              );
       if(results==null) {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
@@ -66,8 +56,9 @@ class _HomeScreenState extends State<HomeScreen> {
       }
       final path = results.files.single.path!;
       final filename=results.files.single.name;
-      print(path);
-      print(filename);
+      storage.
+          uploadFile(path,filename)
+          .then((value)=>print('Done'));
 
     },
     child: Text('upload file'),
