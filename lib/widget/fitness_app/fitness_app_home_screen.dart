2c9911088c0b86f77f5b7be4749f78f2dda31b9d
peter <abcde12345326@gmail.com>
2022-06-19 00:25:21 +0800

    本日記錄

---
+++ lib/widget/fitness_app/fitness_app_home_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'fitness_app_theme.dart';
+import 'models/tabIcon_data.dart';
+import 'my_diary/my_diary_screen.dart';
+
+
+void main() => runApp(const MyApp2());
+
+class MyApp2 extends StatelessWidget {
+  const MyApp2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home:FitnessAppHomeScreen(),
+    );
+  }
+}
+class FitnessAppHomeScreen extends StatefulWidget {
+  @override
+  _FitnessAppHomeScreenState createState() => _FitnessAppHomeScreenState();
+}
+
+class _FitnessAppHomeScreenState extends State<FitnessAppHomeScreen>
+    with TickerProviderStateMixin {
+  AnimationController? animationController;
+
+  List<TabIconData> tabIconsList = TabIconData.tabIconsList;
+
+  Widget tabBody = Container(
+    color: FitnessAppTheme.background,
+  );
+
+  @override
+  void initState() {
+    tabIconsList.forEach((TabIconData tab) {
+      tab.isSelected = false;
+    });
+    tabIconsList[0].isSelected = true;
+
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 600), vsync: this);
+    tabBody = MyDiaryScreen(animationController: animationController);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: FitnessAppTheme.background,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: FutureBuilder<bool>(
+          future: getData(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (!snapshot.hasData) {
+              return const SizedBox();
+            } else {
+              return Stack(
+                children: <Widget>[
+                  tabBody,
+                  // bottomBar(),
+                ],
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 200));
+    return true;
+  }
+}
