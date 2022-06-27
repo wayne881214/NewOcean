60e53e618a94af59ada0073e83b80ce0e0408c8d
Your Name <you@example.com>
2022-06-28 04:18:03 +0800

    commit

---
+++ lib/page/sign_out_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:newocean/screen/auth/auth.dart';
+import 'package:provider/provider.dart';
+
+import 'authentication_service.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text('登出'),
+      centerTitle: true,
+      backgroundColor: Color(0xFF00BFA5),
+    ),
+    body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              onPressed: () {
+                context.read<AuthenticationService>().signOut();
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AuthScreen()));
+              },
+              child: Text("登出"),
+            ),
+          ],
+        ),
+      ),
+  );
+}
+
